7e1a40f30210f5d085094dc86755f2847f6ee9b7
Christian Scorpan <christianscorpan@gmail.com>
2025-02-24 16:04:15 +0100

    add

--- deploy-public_ip.sh
+++ deploy-public_ip.sh
@@ -59,7 +59,7 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \"upgrade\";
         proxy_set_header Host \$host;
-        proxy_read_timeout 3600s;  # Prevent WebSocket timeouts
+        proxy_read_timeout 3600;
     }
 }
 EOF"
